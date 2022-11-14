7c9b9d123dffd6cbddf6afa8b217e35ba443c46e
poornima4321 <117293321+poornima4321@users.noreply.github.com>
2022-11-14 11:41:47 +0000

    variables in python file uploaded

---
+++ main.py
@@ -0,0 +1,42 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    # Variables in Python
+    first_name = 'Poornima'
+    last_name = 'San'
+    country = 'England'
+    city = 'London'
+    age = 21
+    is_married = True
+    skills = ['HTML', 'BIGdata', 'JS', 'React', 'Python']
+    person_info = {
+        'firstname': 'Poornima',
+        'lastname': 'San',
+        'country': 'England',
+        'city': 'London'
+    }
+
+    print('First name:', first_name)
+    print('First name length:', len(first_name))
+    print('Last name: ', last_name)
+    print('Last name length: ', len(last_name))
+    print('Country: ', country)
+    print('City: ', city)
+    print('Age: ', age)
+    print('Married: ', is_married)
+    print('Skills: ', skills)
+    print('Person information: ', person_info)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
